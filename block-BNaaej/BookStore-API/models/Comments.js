870b8b35a49959f1c690684b84e9823423716fe8
Sanaya Arya <sanayaarya0123@gmail.com>
2022-09-30 11:31:29 +0530

    Completed

---
+++ block-BNaaej/BookStore-API/models/Comments.js
@@ -0,0 +1,20 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+    },
+    author: {
+      type: String,
+    },
+    bookRef: {
+      type: Schema.Types.ObjectId,
+      ref: 'Book2',
+    },
+  },
+  { strict: false }
+);
+module.exports = mongoose.model('Comments', commentSchema);
